a2622aea1fc6c8181a93d506424d8c3468d26988
Mohamed El Araby <mohamedalarbic6@gmail.com>
2025-07-22 13:08:50 +0300

    sdf

--- mrmostafa/app.py
+++ mrmostafa/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify, send_file
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify, send_file, make_response
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta, timedelta, date
